9589b3340df50a936e16d561727629915d2c28d8
raibin2 <96141817+raibin2@users.noreply.github.com>
2022-07-27 17:25:32 +0200

    Update primer.py

--- primer.py
+++ primer.py
@@ -63,7 +63,7 @@ print(sum_of_squares(4))
 # give a single command that computes the sum from exercise r-1.4, relying on python's comprehension syntax and built in sum function
 
 def sum_of_squares2(n):
-    return sum([k * k for k in range(0, n)])
+    return sum([k * k for k in range(0, n+1)])
 
 
 print(sum_of_squares2(4))
